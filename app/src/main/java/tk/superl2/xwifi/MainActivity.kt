9b24cdff35af15dc16667f34ef6f8ec526343288
Unknown <joshleivenzon@outlook.com>
2018-03-09 19:25:28 +1100

    Move loading of wifi entries into background and show a loading dialog
    The dialog will look better some time soon, I hope

--- app/src/main/java/tk/superl2/xwifi/MainActivity.kt
+++ app/src/main/java/tk/superl2/xwifi/MainActivity.kt
@@ -2,10 +2,12 @@ package tk.superl2.xwifi
 
 import android.app.Activity
 import android.app.AlertDialog
+import android.app.Dialog
+import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
-import android.widget.ArrayAdapter
-import android.widget.ListView
+import android.view.Gravity
+import android.widget.*
 
 const val TAG = "MainActivity"
 
@@ -18,18 +20,46 @@ class MainActivity: Activity() {
     private lateinit var wifiEntries: ArrayList<WifiEntry>
     private val wifiEntrySSIDs: ArrayList<String> = ArrayList()
     private val wifiEntryPasswords: ArrayList<String> = ArrayList()
+    private val loadWifiEntriesInBackgroundTask = LoadWifiEntriesInBackground()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mWifiListView = findViewById(R.id.wifi_ListView)
+        mWifiListView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, wifiEntrySSIDs)
     }
 
     override fun onResume() {
         super.onResume()
-        loadWifiEntries()
-        mWifiListView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, wifiEntrySSIDs)
+        loadWifiEntriesInBackgroundTask.execute()
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        if (::errorDialog.isInitialized) errorDialog.dismiss()
+        loadWifiEntriesInBackgroundTask.cancel(true)
+        if (::loadingDialog.isInitialized) loadingDialog.dismiss()
+    }
+
+    // This task loads the wifi entries in the background, and notifies the list adapter that the data has changed.
+    lateinit var loadingDialog: Dialog
+    private inner class LoadWifiEntriesInBackground: AsyncTask<Unit, Unit, Unit>() {
+        override fun onPreExecute() {
+            val loadingDialogBuilder = AlertDialog.Builder(this@MainActivity)
+            loadingDialogBuilder.setCancelable(false)
+            loadingDialogBuilder.setMessage(R.string.wifi_loading_message)
+            loadingDialogBuilder.setView(ProgressBar(this@MainActivity))
+            loadingDialog = loadingDialogBuilder.create()
+            loadingDialog.show()
+        }
+        override fun doInBackground(vararg params: Unit?) {
+            loadWifiEntries()
+        }
+        override fun onPostExecute(result: Unit?) {
+            (mWifiListView.adapter as ArrayAdapter<*>).notifyDataSetChanged()
+            loadingDialog.dismiss()
+        }
     }
 
     // This function saves wifi entry data into the wifiEntries ArrayList. It also throws up a dialog if the loading fails.
@@ -37,9 +67,11 @@ class MainActivity: Activity() {
     private lateinit var errorDialog: AlertDialog
     private fun loadWifiEntries() {
         Log.v(TAG, "Loading wifi entries...")
+        if (::wifiEntries.isInitialized) wifiEntries.clear()
+        wifiEntrySSIDs.clear()
+        wifiEntryPasswords.clear()
         try {
             wifiEntries = WifiEntryLoader.readOreoFile()
-            wifiEntrySSIDs.clear()
             wifiEntries.mapTo(wifiEntrySSIDs) { it.title }
             wifiEntries.mapTo(wifiEntryPasswords) { it.getPassword(true) }
             Log.v(TAG, "Wifi entries loaded.")
@@ -62,10 +94,5 @@ class MainActivity: Activity() {
             errorDialog.show()
             Log.v(TAG, "Wifi entries failed to load.")
         }
-    }
-
-    public override fun onStop() {
-        super.onStop()
-        if (::errorDialog.isInitialized) errorDialog.dismiss()
     }
 }
